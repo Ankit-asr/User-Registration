de157f7a43c64d5f3b835a148c2cc26e84276b2e
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-15 15:37:18 +0530

    [Ankit] Add: Check Rule 3 of Password Validation

--- UserRegistrationProblemTest/UnitTest1.cs
+++ UserRegistrationProblemTest/UnitTest1.cs
@@ -6,10 +6,10 @@ namespace UserRegistrationProblemTest
     public class UnitTest1
     {
         [TestMethod]
-        public void GivenMobileNumberCheckShouldBeReturnTrue()
+        public void GivenPasswordCheckShouldReturnTrue()
         {
             UserValidation userValidation = new UserValidation();
-            bool result = userValidation.CheckPhoneNumber("91 9960708015");
+            bool result = userValidation.CheckPassword("ankitSingh8");
             Assert.AreEqual(true, result);
         }
     }
