2d07a421665fa33df45ea3b3155be7d41da70ead
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-15 13:30:12 +0530

    [Ankit] Add: Check First Name Validation

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UserRegistrationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            UserValidation userValidation = new UserValidation();
+            Console.WriteLine("Enter a first name");
+            firstName = Console.ReadLine();
+            if (userValidation.CheckFirstName(firstName))
+                Console.WriteLine(firstName + " : is valid");
+            else
+                Console.WriteLine(firstName + " : is not valid");
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    class UserValidation
+    {
+        public const string FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public bool CheckFirstName(string firstName)
+        {
+            Regex regex = new Regex(FIRST_NAME);
+            return regex.IsMatch(firstName);
+        }
+    }
+}
