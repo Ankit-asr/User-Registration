7baf3429a446eefa2e1fd1ad153c39e91d7aa508
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-15 14:00:03 +0530

    [Ankit] Add: Check Last Name Validation

--- UserRegistrationProblemTest/UnitTest1.cs
+++ UserRegistrationProblemTest/UnitTest1.cs
@@ -1,16 +1,1 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using UserRegistrationProblem;
-namespace UserRegistrationProblemTest
-{
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void GivenLastNameWhenCheckShouldBeReturnTrue()
-        {
-            UserValidation userValidation = new UserValidation();
-            bool result = userValidation.CheckLastName("Pkash");
-            Assert.AreEqual(true, result);
-        }
-    }
-}
+
